143fe8c8033487c7fcb09e84e3f5d8e5d404e4b4
angelovangel <aangeloo@gmail.com>
2023-03-30 15:23:36 +0300

    reactive values fix

--- app.R
+++ app.R
@@ -11,10 +11,10 @@ library(shinydashboard)
 
 # creates base empty dataframe to view and fill later
 make_dest <- function() {
-  dest <- tibble(src_type = as.character(NA), 
-                 src_well = wells_colwise, 
-                 sample_name = as.character(NA), 
-                 customer_primer = as.character(NA), 
+  dest <- tibble(src_type = 'plate', 
+                 src_well = "", 
+                 sample_name = "", 
+                 customer_primer = "", 
                  bcl_primer = as.character(NA), 
                  dest_well = wells_colwise)
                  #mycolor = NA)
@@ -32,7 +32,7 @@ tab1 <- fluidRow(
                tags$p("Reaction plate preview"),
                reactableOutput('plate'),
                tags$p('hot preview'),
-               reactableOutput('hot_preview'))
+               verbatimTextOutput('hot_preview'))
       ))
 )
 
@@ -85,21 +85,33 @@ server = function(input, output, session) {
   # CORE 
   myvalues <- reactive({
     # use only ones where there is sample name and source type selected
+    # this avoids a bug where deleting a well did not leave an empty entry in the vector
+    sourcewells1 <- rep('', 96)
+    sourcewells1[hot()$src_type == 'plate'] <- hot()$src_well
     
-    sourcewells1 <- hot()$src_well[hot()$sample_name != '' & hot()$src_type == 'plate'] %>% str_replace_na(replacement = ' ')
-    sourcewells2 <- hot()$src_well[hot()$sample_name != '' & hot()$src_type == 'strip'] %>% str_replace_na(replacement = ' ')
+    sourcewells2 <- rep('', 96)
+    sourcewells2[hot()$src_type == 'strip'] <- hot()$src_well
+    
     # this took  while to figure out, the length of the result is the same as the first arg of match
     sourcewells3 <- bcl_primers$primer_well[match(hot()$bcl_primer, bcl_primers$primer_name)] %>% str_replace_na(replacement = ' ')
-     
-    volume1 <- rep(10, 96)[hot()$sample_name != '' & hot()$src_type == 'plate'] %>% str_replace_na(replacement = '0')
-    volume2 <- rep(10, 96)[hot()$sample_name != '' & hot()$src_type == 'strip'] %>% str_replace_na(replacement = '0')
+    
+    # when something is deleted in hot() it becomes ''
+    volume1 <- rep(0, 96)
+    volume1[hot()$bcl_primer != ''] <- 10
+    volume1[hot()$customer_primer != ''] <- 15
+    
+    
+    volume2 <- rep(0, 96)
+    volume2[hot()$bcl_primer != ''] <- 10
+    volume2[hot()$customer_primer != ''] <- 15
+    
     volume3 <- rep(5, 96)[hot()$sample_name != '' & hot()$bcl_primer != ''] %>% str_replace_na(replacement = '0') 
     
-    premix_pos1 <- which(hot()$customer_primer != '' & hot()$sample_name != '' & hot()$src_type == 'plate')
-    volume1[premix_pos1] <- 15
-    
-    premix_pos2 <- which(hot()$customer_primer != '' & hot()$sample_name != '' & hot()$src_type == 'strip')
-    volume2[premix_pos2] <- 15
+    # premix_pos1 <- which(hot()$customer_primer != '' & hot()$sample_name != '' & hot()$src_type == 'plate')
+    # volume1[premix_pos1] <- 15
+    # 
+    # premix_pos2 <- which(hot()$customer_primer != '' & hot()$sample_name != '' & hot()$src_type == 'strip')
+    # volume2[premix_pos2] <- 15
     
     # set these to 15 when customer primer is used
     #volume1[!is.na(hot()$customer_primer)] <- 15
@@ -133,7 +145,8 @@ server = function(input, output, session) {
   output$hot <- renderRHandsontable({
     rhandsontable(hot(),
                   rowHeaders = NULL, height = 2800, stretchH = "all") %>%
-      hot_col(col = 'src_type', type = 'autocomplete', source = c('strip', 'plate'), strict = T) %>%
+      hot_col(col = 'src_type', type = 'dropdown', source = c('strip', 'plate'), strict = T) %>%
+      hot_validate_character('src_type', choices = c('strip', 'plate'), allowInvalid = F) %>%
       hot_col(col = 'src_well', type = 'dropdown', source = wells_colwise, strict = T) %>%
       hot_col(col = 'bcl_primer', type = 'autocomplete', source = bcl_primers$primer_name) %>%
       hot_col(col = 'dest_well', readOnly = T) %>%
@@ -152,8 +165,9 @@ server = function(input, output, session) {
               )
   })
   
-  output$hot_preview <- renderReactable({
-    reactable(hot())
+  output$hot_preview <- renderText({
+    #hot()$src_well
+    myvalues()[2]
   })
   
   output$protocol_preview <- renderPrint({
